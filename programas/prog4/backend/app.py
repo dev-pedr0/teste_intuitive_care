bb7980e5514afeee47b76926d1d984cc051b90df
Pedro Costa <pedro4costa@outlook.com>
2026-02-02 12:26:00 -0300

    primeira rota finalizada

--- programas/prog4/backend/app.py
+++ programas/prog4/backend/app.py
@@ -1,13 +1,43 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from db import conectar_banco
 
 app = FastAPI(title="API Operadoras de Saúde")
 
-@app.get("/db-test")
-def testar_banco():
-    try:
-        conn = conectar_banco()
-        conn.close()
-        return {"status": "conectado com sucesso"}
-    except Exception as e:
-        return {"erro": str(e)}
+@app.get("/api/operadoras")
+def listar_operadoras(
+    page: int = Query(1, ge=1),
+    limit: int = Query(10, ge=1, le=100)
+):
+    #Páginas
+    offset = (page - 1) * limit
+
+    #Conecta ao banco
+    conn = conectar_banco()
+    cursor = conn.cursor(dictionary=True)
+
+    #Total de registros
+    cursor.execute("SELECT COUNT(*) AS total FROM cadastro_operadoras")
+    total = cursor.fetchone()["total"]
+
+    #Dados paginados
+    cursor.execute(
+        """
+        SELECT cnpj, registro_operadora, modalidade, uf, status_cadastro
+        FROM cadastro_operadoras
+        LIMIT %s OFFSET %s
+        """,
+        (limit, offset)
+    )
+    operadoras = cursor.fetchall()
+
+    #Desconecta
+    cursor.close()
+    conn.close()
+
+    #Retornar dados necessários
+    return {
+        "data": operadoras,
+        "total": total,
+        "page": page,
+        "limit": limit
+    }
